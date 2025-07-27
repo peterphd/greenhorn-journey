a3503cb2d5380f257c50e14d9580d4b4e73de278
Adamu Peter <peterphdadamu@gmail.com>
2025-07-27 09:56:02 +0100

    Add remaining programs from variables_if_else_while project

---
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void){
+	putchar('z');
+	putchar('y');
+	putchar('x');
+	putchar('w');
+	putchar('v');
+	putchar('u');
+	putchar('t');
+	putchar('s');
+	putchar('r');
+	putchar('q');
+	putchar('p');
+	putchar('o');
+	putchar('n');
+	putchar('m');
+	putchar('l');
+	putchar('k');
+	putchar('j');
+	putchar('i');
+	putchar('h');
+	putchar('g');
+	putchar('f');
+	putchar('e');
+	putchar('d');
+	putchar('c');
+	putchar('b');
+	putchar('a');
+	putchar('\n');
+
+	return 0;
+}
