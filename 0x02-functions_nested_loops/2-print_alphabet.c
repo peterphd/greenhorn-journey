adcdb4b19b67eb3649e9e0ffa3419ef515cc0db3
Adamu Peter <peterphdadamu@gmail.com>
2025-07-28 06:16:35 +0100

    Add task 2: print_alphabet function
    - Prints lowercase alphabet once (a-z)
    - Single loop implementation
    - Followed by newline

---
+++ 0x02-functions_nested_loops/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the letters of the alphabet in lower case, followed by a new line.
+ *
+ * Return: void
+ */
+
+void print_alphabet(void){
+	char ch;
+	for (ch='a'; ch<='z'; ch++){
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+
+	
+}
